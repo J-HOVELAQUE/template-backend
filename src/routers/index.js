8e9d0a93d3aedbc961188446c893b163d8b835d9
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-06 10:14:17 +0100

    pouet

--- src/routers/index.js
+++ src/routers/index.js
@@ -7,4 +7,9 @@ const dataBaseTest = require('../controllers/dataBaseTest');
 router.get('/', home);
 router.get('/db/test', dataBaseTest);
 
+router.post('/transponder', function (req, res) {
+    console.log(req.body);
+    res.json({ message: 'OK' })
+})
+
 module.exports = router;
